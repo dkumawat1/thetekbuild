1a67e11f431772fd2700804211409cf28fed228e
dino1101 <dinesh.k@sunedison.in>
2023-02-09 11:18:55 +0530

    first build

---
+++ build/precache-manifest.a412b8ebaeb9224cc0a93a833b3f1319.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc8aec31f17667af4edc",
+    "url": "/static/css/main.59d5e64d.chunk.css"
+  },
+  {
+    "revision": "fc8aec31f17667af4edc",
+    "url": "/static/js/main.fc8aec31.chunk.js"
+  },
+  {
+    "revision": "ec5ddaa9f6f0a593122a",
+    "url": "/static/js/1.ec5ddaa9.chunk.js"
+  },
+  {
+    "revision": "bf5a9897cc00b55a5e9d",
+    "url": "/static/css/2.aef3f0d3.chunk.css"
+  },
+  {
+    "revision": "bf5a9897cc00b55a5e9d",
+    "url": "/static/js/2.bf5a9897.chunk.js"
+  },
+  {
+    "revision": "e01dcd01bb42adeaa836",
+    "url": "/static/js/runtime~main.e01dcd01.js"
+  },
+  {
+    "revision": "a55f97a5c5d0c7d3a5f5418b8c1a02b4",
+    "url": "/index.html"
+  }
+];
